fb5d9a770993535bc49006e87884112f744ef64c
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-11-21 19:34:19 -0500

    Step 6 (Final) - Score
    The scoreboard and floating scores now work. The animation for the
    floating score is working but it isn't centered with the layout. Editing
    the rect transform from the inspector wasn't working so I wasn't sure
    where it is going wrong.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -15,6 +15,10 @@ public class Prospector : MonoBehaviour {
 	public float xOffset = 3;
 	public float yOffset = -2.5f;
 	public Vector3 layoutCenter;
+	public Vector2 fsPosMid = new Vector2( 0.5f, 0.90f );
+	public Vector2 fsPosRun = new Vector2( 0.5f, 0.75f );
+	public Vector2 fsPosMid2 = new Vector2( 0.4f, 1.0f );
+	public Vector2 fsPosEnd = new Vector2( 0.5f, 0.95f );
 
 	[Header ("Set Dynamically")]
 	public Deck					deck;
@@ -24,12 +28,15 @@ public class Prospector : MonoBehaviour {
 	public CardProspector target;
 	public List<CardProspector> tableau;
 	public List<CardProspector> discardPile;
+	public FloatingScore fsRun;
 
 	void Awake(){
 		S = this;
 	}
 
 	void Start() {
+		Scoreboard.S.score = ScoreManager.SCORE;
+
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards); // shuffles the deck by reference
@@ -138,6 +145,7 @@ public class Prospector : MonoBehaviour {
 		cd.SetSortingLayerName(layout.discardPile.layerName);
 		cd.SetSortOrder(-100+discardPile.Count);
 	}
+
 	// Make cd the new target card
 	void MoveToTarget(CardProspector cd) {
 		// If there is currently a target card, move it to discardPile
@@ -155,6 +163,7 @@ public class Prospector : MonoBehaviour {
 		cd.SetSortingLayerName(layout.discardPile.layerName);
 		cd.SetSortOrder(0);
 	}
+
 	// Arranges all the cards of the drawPile to show how many are left
 	void UpdateDrawPile() {
 		CardProspector cd;
@@ -198,6 +207,8 @@ public class Prospector : MonoBehaviour {
 			MoveToDiscard (target); // Moves the target to the discardPile
 			MoveToTarget (Draw ()); // Moves the next drawn card to the target
 			UpdateDrawPile (); // Restacks the drawPile
+			ScoreManager.EVENT(eScoreEvent.draw);
+			FloatingScoreHandler(eScoreEvent.draw);
 			break;
 		case eCardState.tableau:
 			// Clicking a card in the tableau will check if it's a valid play
@@ -216,39 +227,46 @@ public class Prospector : MonoBehaviour {
 			tableau.Remove (cd); // Remove it from the tableau List
 			MoveToTarget (cd); // Make it the target card
 			SetTableauFaces(); // Update tableau card face-ups
+			ScoreManager.EVENT(eScoreEvent.mine);
+			FloatingScoreHandler(eScoreEvent.mine);
 			break;
 		}
 	// Check to see whether the game is over or not
 	CheckForGameOver();
 }
-// Test whether the game is over
-void CheckForGameOver() {
-	// If the tableau is empty, the game is over
-	if (tableau.Count==0) {
-		// Call GameOver() with a win
-		GameOver(true);
-		return;
-	}
-	// If there are still cards in the draw pile, the game's not over
-	if (drawPile.Count>0) {
-		return;
-	}
-	// Check for remaining valid plays
-	foreach ( CardProspector cd in tableau ) {
-		if (AdjacentRank(cd, target)) {
-			// If there is a valid play, the game's not over
+	// Test whether the game is over
+	void CheckForGameOver() {
+		// If the tableau is empty, the game is over
+		if (tableau.Count==0) {
+			// Call GameOver() with a win
+			GameOver(true);
 			return;
 		}
+		// If there are still cards in the draw pile, the game's not over
+		if (drawPile.Count>0) {
+			return;
+		}
+		// Check for remaining valid plays
+		foreach ( CardProspector cd in tableau ) {
+			if (AdjacentRank(cd, target)) {
+				// If there is a valid play, the game's not over
+				return;
+			}
+		}
+		// Since there are no valid plays, the game is over
+		// Call GameOver with a loss
+		GameOver (false);
 	}
-	// Since there are no valid plays, the game is over
-	// Call GameOver with a loss
-	GameOver (false);
-}
+
 	void GameOver(bool won) {
 		if (won) {
-			print ("Game Over. You won! :)");
+			// print ("Game Over. You won! :)"); // Comment out this line
+			ScoreManager.EVENT(eScoreEvent.gameWin);
+			FloatingScoreHandler(eScoreEvent.gameWin);
 		} else {
-			print ("Game Over. You Lost. :(");
+			// print ("Game Over. You Lost. :("); // Comment out this line
+			ScoreManager.EVENT(eScoreEvent.gameLoss);
+			FloatingScoreHandler(eScoreEvent.gameWin);
 		}
 		// Reload the scene, resetting the game
 		SceneManager.LoadScene("__Prospector_Scene_0");
@@ -268,4 +286,50 @@ void CheckForGameOver() {
 			// Otherwise, return false
 			return(false);
 		}
+
+	// Handle FloatingScore movement
+	void FloatingScoreHandler(eScoreEvent evt) {
+		List<Vector2> fsPts;
+		switch (evt) {
+		// Same things need to happen whether it's a draw, a win, or a loss
+		case eScoreEvent.draw: // Drawing a card
+		case eScoreEvent.gameWin: // Won the round
+		case eScoreEvent.gameLoss: // Lost the round
+			// Add fsRun to the Scoreboard score
+			if (fsRun != null) {
+				// Create points for the Bézier curve1
+				fsPts = new List<Vector2>();
+				fsPts.Add( fsPosRun );
+				fsPts.Add( fsPosMid2 );
+				fsPts.Add( fsPosEnd );
+				fsRun.reportFinishTo = Scoreboard.S.gameObject;
+				fsRun.Init(fsPts, 0, 1);
+				// Also adjust the fontSize
+				fsRun.fontSizes = new List<float>(new float[] {28,36,4});
+				fsRun = null; // Clear fsRun so it's created again
+			}
+			break;
+		case eScoreEvent.mine: // Remove a mine card
+			// Create a FloatingScore for this score
+			FloatingScore fs;
+			// Move it from the mousePosition to fsPosRun
+			Vector2 p0 = Input.mousePosition;
+			p0.x /= Screen.width;
+			p0.y /= Screen.height;
+			fsPts = new List<Vector2> ();
+			fsPts.Add (p0);
+			fsPts.Add (fsPosMid);
+			fsPts.Add (fsPosRun);
+			fs = Scoreboard.S.CreateFloatingScore(ScoreManager.CHAIN, fsPts);
+			fs.fontSizes = new List<float>(new float[] {4,50,28});
+			if (fsRun == null) {
+				fsRun = fs;
+				fsRun.reportFinishTo = null;
+			} else {
+				fs.reportFinishTo = fsRun.gameObject;
+			}
+			break;
+		}
+	}
+
 }
